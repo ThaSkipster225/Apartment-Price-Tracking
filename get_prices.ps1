c82d422920acf741022c41a2c91d301cf032acad
ThaSkipster225 <98471613+ThaSkipster225@users.noreply.github.com>
2025-08-06 22:31:12 -0700

    Added PowerShell script to do all set up and run the Python script in line in command line.

---
+++ get_prices.ps1
@@ -0,0 +1,5 @@
+cd "C:\Users\skips\Documents\code\Python\Testing\Responses\Apartment-Price-Tracking";
+
+C:\Users\skips\Documents\code\Python\VirtualEnvironment\scripts\activate.ps1;
+
+py .\dobsonRanchPricing.py;
